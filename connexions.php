881f3825c748ea935dd4a98b8eea851746a04121
Akashsurendren <akash.surendren@student.helb-prigogine.be>
2026-03-27 14:21:39 +0100

    Petites modifications

--- connexions.php
+++ connexions.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once('db.php'); // Toujours appeler le traducteur en premier
-
+include('design.php');
 // 1. DÉCONNEXION
 if (isset($_GET['logout'])) {
     session_destroy();
